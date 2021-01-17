413f5111dca3039c23519e8cd65d00496545def2
arfranzim <andrefranzim@gmail.com>
2021-01-17 01:22:21 -0300

    subindo pra iniciar aws

---
+++ forum/src/main/java/br/com/franzim/forum/controller/TopicosController.java
@@ -0,0 +1,32 @@
+package br.com.franzim.forum.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+
+@RestController
+public class TopicosController {
+
+	@RequestMapping("/topicos")
+	public List<Topico> lista( @RequestParam(value = "name", defaultValue = "World") String name) {
+		
+		Curso curso = new Curso("Spring","Programação");
+		
+		if(curso.getNome().equals(name)) {
+			Topico topico = new Topico("Dúvida", name, curso);
+			Topico topico2 = new Topico("Dúvida 2", "Dúvida no Spring", curso);
+			Topico topico3 = new Topico("Dúvida 3", "Dúvida no Spring", curso);
+			return Arrays.asList(topico, topico2, topico3);
+		} else {
+			return null;
+		}
+		
+		
+	}
+}
--- forum/src/main/java/br/com/franzim/forum/controller/ViewController.java
+++ forum/src/main/java/br/com/franzim/forum/controller/ViewController.java
@@ -1,14 +1,12 @@
 package br.com.franzim.forum.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ViewController {
 
 	@RequestMapping("/")
-	@ResponseBody
 	public String init() {
 		return "Response body";
 	}
